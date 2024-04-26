2c0425207928bcc1e6777901ff97c26414a4eea7
Aidan Pantoya <111801426+aidan-pantoya@users.noreply.github.com>
2024-04-26 11:16:48 -0500

    Update L1-3_merge_housekeeping_files.py

--- PINE-3_WTAMU_processing/L1-3_merge_housekeeping_files.py
+++ PINE-3_WTAMU_processing/L1-3_merge_housekeeping_files.py
@@ -2,8 +2,10 @@ import time
 import glob
 import natsort
 import pandas as pd
-    
-hlist = glob.glob('F:/ExINPNSA/ExINPNSA_WTAMU/2/df_PINE-3_ExINPNSA21_opid*.txt')
+
+# THIS CODE MERGES L1-2 HOUSEKEEPING FILES
+
+hlist = glob.glob('F:/ExINPNSA/ExINPNSA_WTAMU/2/df_PINE-3_ExINPNSA21_opid*.txt') # DIRECTORY TO ALL FILES CREATED IN L1-2, HOUSEKEEPING FILES
 hlist = natsort.natsorted(hlist)   
 start = time.time()
 
@@ -20,4 +22,5 @@ end = time.time()
 
 print(f'Time elapsed: {end-start} seconds')
 
+# CREATE 3 FOLDER, CHANGE DIRECTORY AND FILENAME 
 h1.to_csv('F:/ExINPNSA/ExINPNSA_WTAMU/3/df_PINE-3_ExINPNSA21_opid-Reference_MEASUREMENTSMERGED_instrument_L1.txt', header=True, index=False, sep = '\t')
