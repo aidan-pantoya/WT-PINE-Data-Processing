931585b3f17f0f54e754e56e299774187abe1dbb
Aidan Pantoya <111801426+aidan-pantoya@users.noreply.github.com>
2024-04-26 10:43:48 -0500

    Update L1-1_generate_threshold_images_NSA.py

--- PINE-3_WTAMU_processing/L1-1_generate_threshold_images_NSA.py
+++ PINE-3_WTAMU_processing/L1-1_generate_threshold_images_NSA.py
@@ -10,29 +10,32 @@ from IPython import get_ipython
 get_ipython().magic('reset -sf')
 
 def tsyncNthresh(flist, calib):
-    col_names1 = ['Date_Time', 'Unknown_Value', 'Channel#', 'Pulse', 'Dp', 'Particle_Type']
-    col_names2 = ['Channel#', 'Dp']
-    dtype1 = {'Unknown_Value': np.float16, 'Pulse': np.float16, 'Channel#': np.uint16, 'Dp': np.float16, 'Particle_Type': 'category'}
-    calib_data = pd.read_csv(calib, sep = '\t', names=col_names2, header=None, dtype = {'Channel#':np.uint16, 'Dp':np.float16})
-    flist = natsort.natsorted(flist)     
-    fig, (ax1) = plt.subplots(1, 1, sharex=True, figsize=(17, 10))
-    fig.autofmt_xdate()
-    xfmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
+    col_names1 = ['Date_Time', 'Unknown_Value', 'Channel#', 'Pulse', 'Dp', 'Particle_Type'] # HEADER NAMES OF FILE
+    col_names2 = ['Channel#', 'Dp'] # CALIBRATION HEADER
+    dtype1 = {'Unknown_Value': np.float16, 'Pulse': np.float16, 'Channel#': np.uint16, 'Dp': np.float16, 'Particle_Type': 'category'} # SETTING TYPES FOR CONVERTING F100 FILES WITH CALIBRATION
+    calib_data = pd.read_csv(calib, sep = '\t', names=col_names2, header=None, dtype = {'Channel#':np.uint16, 'Dp':np.float16}) # READING IN CALIBRATION FILES
+    flist = natsort.natsorted(flist)  # SORTING ALL F100 FILES
+    fig, (ax1) = plt.subplots(1, 1, sharex=True, figsize=(17, 10)) # SETTING UP GRAPH
+    fig.autofmt_xdate() # CREATING DATES FOR GRAPH
+    xfmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S') # FORMATTING THE DATES
 
-    for f in flist:
-        print(f)
-        new_title1 = os.path.splitext(os.path.basename(f))[0]
+    for f in flist: # GOES THROUGH EACH FILE IN F1000 OPID LIST
+        print(f) # PRINTS WHICH FILE WE ARE ON
+        new_title1 = os.path.splitext(os.path.basename(f))[0]  # GET A TITLE FOR GRAPH
+
+
 
         fpd = pd.read_csv(f, sep='\t', header=None, names=col_names1, usecols=['Date_Time', 'Channel#', 'Dp', 'Particle_Type'],
-                          encoding = 'unicode_escape', engine='python', dtype=dtype1)
-        fpd['Date_Time'] = pd.to_datetime(fpd['Date_Time']) #- pd.to_timedelta(104, unit='s')
+                          encoding = 'unicode_escape', engine='python', dtype=dtype1) # READ IN THE CURRENT FILE
+
+        fpd['Date_Time'] = pd.to_datetime(fpd['Date_Time']) #- pd.to_timedelta(104, unit='s') # USED IF YOU NEED TO CONVERT THE DATE
 
 
-        for i in calib_data.index:
+        for i in calib_data.index: # LOOP FOR CONVERTING THE PARTICLE CHANNELS AND Dps
             mask1 = calib_data.loc[i, 'Channel#'] == fpd['Channel#']
             fpd.loc[mask1, 'Dp'] = calib_data.loc[i, 'Dp']
 
-        fpd.columns = ['Date_Time', 'Channel#', 'Dp', 'Particle_Type']
+        fpd.columns = ['Date_Time', 'Channel#', 'Dp', 'Particle_Type'] # plotting that information
         ax1.plot(np.array(fpd['Date_Time']), np.array(fpd['Dp']),'.',color = 'black')
         ax1.xaxis.set_major_formatter(xfmt)                                             
         ax1.tick_params(direction='in', bottom=True, top=True, left=True, right=True)
@@ -44,20 +47,18 @@ def tsyncNthresh(flist, calib):
             plt.setp(ax1.get_xticklabels(), rotation=-45)
             plt.suptitle(''+ new_title1 +'')
 
-            plt.savefig('F:/ExINPNSA/ExINPNSA_WTAMU/plots/' + new_title1 + '_L1.png', format='png', dpi=800)
+            plt.savefig('F:/ExINPNSA/ExINPNSA_WTAMU/plots/' + new_title1 + '_L1.png', format='png', dpi=800) # saving the figure, YOU WILL NEED TO CHANGE THE DIRECTORY
 
     return
 
-#  Calibration file(welas-133)
+#  Calibration file(welas-133). YOU WILL NEED CHANGE DIRECTORY FOR YOUR CALIBRATION FILE
 calib = "F:/PINE_Python/Codes/welasc1_1_cal_range4_133.txt"
 
-# 165, 210, 223, 253, 283, 294, 364, 374, 391, 528, 553, 576, 614, 651, 665, 670, \
-#    683, 697, 700, 729, 822, 835, 894, 979, 1067,1075,1153,1170,1171,1178,1193,1195, \
-#      1204, 1210, 1253, 1303, 1347  
-
+# CHANGE THE OPIDS FOR YOUR OPERATIONS
 opids = [1253]
 
-for i in range(1,1236):
-    flist = glob.glob('F:/ExINPNSA/ExINPNSA21/L0_Data/f100-01/df_PINE-3_ExINPNSA21_opid-'+str(i)+'_runid-*_.txt')
+for i in opids: # this can be used for running multiple OPIDS
+    # CHANGE DIRECTORY TO F100 RUN-ID FILES, NAME SHOULD END WITH: _opid-'+str(i)+'_runid-*_.txt'
+    flist = glob.glob('F:/ExINPNSA/ExINPNSA21/L0_Data/f100-01/df_PINE-3_ExINPNSA21_opid-'+str(i)+'_runid-*_.txt') 
    
 tsyncNthresh(flist, calib)
